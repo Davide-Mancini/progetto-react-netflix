eaae7da83e1cdaee541cfa492d14ccbae89a3e2e
Davide-Mancini <mancinidavide73@gmail.com>
2025-07-25 17:00:25 +0200

    adds

--- src/components/MySetting.jsx
+++ src/components/MySetting.jsx
@@ -1,4 +1,12 @@
-import { Col, Container, Row, InputGroup, Form } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Row,
+  InputGroup,
+  Form,
+  Dropdown,
+  Button,
+} from "react-bootstrap";
 
 const MySetting = () => {
   return (
@@ -25,6 +33,30 @@ const MySetting = () => {
                     aria-describedby="inputGroup-sizing-default"
                   />
                 </InputGroup>
+                <p>Language:</p>
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    English
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Something else
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                <hr />
+                <p>Maturity Ratings</p>
+                <Button>ALL MATURITY RATINGS</Button>
+                <small>
+                  Show title of all maturities ratings for this profile
+                </small>
+                <Button>EDIT</Button>
+                <hr />
               </Col>
             </Row>
           </Col>
